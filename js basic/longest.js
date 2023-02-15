29f51532fdcfd24f7eca97483c86b5f8ccd23e43
Dharmendra Kumar <57281417+DharmendraGITHB@users.noreply.github.com>
2023-02-15 22:31:51 +0530

    longest

---
+++ js basic/longest.js
@@ -0,0 +1,9 @@
+// Find the longest word in a string
+
+function findLongestWordLength(str) {
+  return str.length;
+}
+
+let long = findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+console.log(long)
