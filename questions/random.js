91acfb7e2710c3c4afbc002cf368428b50a2322e
Dharmendra Kumar <57281417+DharmendraGITHB@users.noreply.github.com>
2023-02-17 11:58:53 +0530

    random number

---
+++ questions/random.js
@@ -0,0 +1,18 @@
+// generating a random number between 0 to 1
+
+let a = Math.random();
+console.log("random number between 0 to 1: " + a);
+
+/* 
+Math.random() *(highestNumber - lowerNumber) + lowestNumber 
+ 
+ this formula is used to find random number between any two range
+*/
+
+//random between 1 to 10
+let b = Math.random()*(10-1) +1
+console.log("random number between 1 to 10: " + b)
+
+// random between 1 to 100
+let c = Math.random()*(100-1) +1
+console.log("random number between 1 to 100: "+ c)
